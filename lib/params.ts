f320c79ab9c2c2dcf4bf48d4e012c41b50aa9ec3
Vincent Njoku <vnjoku@amazon.com>
2023-03-08 09:47:52 -0600

    updated vars

--- lib/params.ts
+++ lib/params.ts
@@ -1,9 +1,9 @@
 export const Params = {
-    GITHUB_REPO: "",
-    BRANCH_NAME: "",
-    GITHUB_TOKEN: "",
-    AWS_REGION: "",
-    DEV_ACCOUNT_ID: "",
-    STAGING_ACCOUNT_ID: "",
-    PROD_ACCOUNT_ID: ""
+    GITHUB_REPO: "vincentnjoku/awsomebuild",
+    BRANCH_NAME: "main",
+    GITHUB_TOKEN: "token-value",
+    AWS_REGION: "us-east-1",
+    DEV_ACCOUNT_ID: "837309331524",
+    STAGING_ACCOUNT_ID: "039087290997",
+    PROD_ACCOUNT_ID: "039087290997"
 } as const
